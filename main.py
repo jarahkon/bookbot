f33886f30320a40b48498595ac749343facebcc2
jarahkon <10908382+jarahkon@users.noreply.github.com>
2025-04-29 16:21:19 +0300

    final changes

--- main.py
+++ main.py
@@ -1,23 +1,12 @@
 from stats import get_number_of_words, get_each_character_count, get_sorted_dicts
+import sys
 
 def get_book_text(book_path: str) -> str:
-    """
-    Reads the content of a book file and returns it as a string.
-    
-    Args:
-        book_path (str): The path to the book file.
-        
-    Returns:
-        str: The content of the book file.
-    """
     with open(book_path, 'r', encoding='utf-8') as file:
         file_contents = file.read()
     return file_contents
 
 def print_report(word_count: int, sorted_dicts: list, book_path: str):
-    """
-    Prints a report of the word and character counts.
-    """
     print("============ BOOKBOT ============")
     print(f"Analyzing book found at: {book_path}...")
     print("----------- Word Count ----------")
@@ -29,14 +18,15 @@ def print_report(word_count: int, sorted_dicts: list, book_path: str):
     print("============= END ===============")
 
 def main():
-    """
-    Main function to execute the script.
-    """
-    book_path = 'books/frankenstein.txt'  # Replace with your book file path
+    if len(sys.argv) < 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    book_path = sys.argv[1]
     book_text = get_book_text(book_path)
     word_count = get_number_of_words(book_text)
     character_count = get_each_character_count(book_text)
     list_of_dicts = get_sorted_dicts(character_count)
     print_report(word_count, list_of_dicts, book_path)
+
 if __name__ == "__main__":
     main()
--- stats.py
+++ stats.py
@@ -1,26 +1,8 @@
 def get_number_of_words(text: str) -> int:
-    """
-    Counts the number of words in a given text.
-    
-    Args:
-        text (str): The text to count words in.
-        
-    Returns:
-        int: The number of words in the text.
-    """
     words = text.split()
     return len(words)
 
 def get_each_character_count(text: str) -> dict:
-    """
-    Counts the occurrences of each character in a given text.
-    
-    Args:
-        text (str): The text to count characters in.
-        
-    Returns:
-        dict: A dictionary with characters as keys and their counts as values.
-    """
     character_dict = {}
     book_text_lower = text.lower()
     for character in book_text_lower:
